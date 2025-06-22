7f6f224d9041e5193046b6b03c25344909626883
Daniel Wennström <daniel@wennstrom.io>
2025-06-22 19:09:27 +0200

    Added participants field to MeetingDto

--- src/main/java/org/example/meetingcalendarapi/dto/MeetingDto.java
+++ src/main/java/org/example/meetingcalendarapi/dto/MeetingDto.java
@@ -1,11 +1,19 @@
 package org.example.meetingcalendarapi.dto;
 
 import lombok.Data;
-import org.example.meetingcalendarapi.model.Meeting;
+import org.example.meetingcalendarapi.enums.MeetingLevel;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Data
-public class MeetingDto extends Meeting {
+public class MeetingDto {
+    private Long id;
+    private String title;
+    private String description;
+    private ZonedDateTime dateTime;
+    private MeetingLevel level;
+    private UserDto creator;
+    private List<UserDto> participants;
     private List<Long> participantIds;
 }
