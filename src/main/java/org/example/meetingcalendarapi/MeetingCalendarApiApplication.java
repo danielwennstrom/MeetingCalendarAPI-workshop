2bf8705e925711c985fd5ebc866114ee044ca96a
danielwennstrom <daniel@wennstrom.io>
2025-06-25 15:34:37 +0200

    Remove dotenv import

--- src/main/java/org/example/meetingcalendarapi/MeetingCalendarApiApplication.java
+++ src/main/java/org/example/meetingcalendarapi/MeetingCalendarApiApplication.java
@@ -1,6 +1,5 @@
 package org.example.meetingcalendarapi;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
